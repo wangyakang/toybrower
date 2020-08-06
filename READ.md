4c7ec5c3f0d2935e707afd028577abb2f42625e1
unknown <G006212@china-goldcard.com>
2020-08-06 21:00:45 +0800

    0806

---
+++ READ.md
@@ -0,0 +1,14 @@
+# 写一个属于自己的浏览器
+
+## 浏览器工作原理
+   + 
+## 目录结构
+   + http : 实现http  request-response 过程
+   + server.js:  使用Node开启一个简单的服务器
+   // HTTP/1.1 200 OK
+        // Content-Length: 3059
+        // Server: GWS/2.0
+        // 
+        // <html>
+        //    <div>hello word</div>
+        // <html/>
--- http/http.js
+++ http/http.js
@@ -1,8 +1,11 @@
+var net = require('net');
+const { rejects } = require('assert');
+
 /**
  * @describe http请求
  */
 
- class Request {
+class Request {
     constructor (options = {}) {
         this.method = options.method || 'get';
         this.path = options.path || '/';
@@ -10,16 +13,59 @@
         this.post = options.post || '8080';
         this.headers = options.headers || {};
         this.body = options.body || {};
+        this.bodyText = '';
 
-        if (this.headers)
+        if (!this.headers['Content-Type']) {
+            this.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+        }
+
+        let contentType = this.headers['Content-Type'];
+        if (contentType === 'application/json') {
+            this.bodyText = JSON.stringify(this.body);
+        } else if (contentType === 'application/x-www-form-urlencoded') {
+            this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&');
+        }
+        this.headers['Content-Length'] = this.bodyText.length;
     }
- }
+    send (connection) {
+        return new Promise (_ => {
+            if (connection) {
+                connection.write(this.resToString());
+            } else {
+                connection = net.createConnection({
+                    post: this.post,
+                    host: this.host
+                }, _ => {
+                    connection.write(this.resToString());
+                })
 
-async (function () {
+            }
+            connection.on('data', data => {
+                console.log(data.toString(), 999);
+                // ...
+            })
+            connection.on('error', err => {
+                console.error(err);
+                rejects(err);
+                connection.end();
+            })
+        })
+    }
+    
+    resToString () { // 拼接request格式
+        // GET / HTTP/1.1           // request line
+        // Host: www.google.com     // headers
+        // 
+        // name=wyk&sex=1           // body
+        return `${this.method} ${this.path} HTTP\r\n ${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r\n\r\n${this.bodyText}`
+    }
+}
+
+async function httpserver() {
     let request = new Request({
-        method: 'get',
+        method: 'POST',
         path: '/',
-        host: '127.1.1.1',
+        host: 'localhost',
         port: '8080',
         headers: {
             'Accept': '*'
@@ -30,4 +76,5 @@ async (function () {
     });
     let response = await request.send();
     console.log(response, 'Response back body');
-})()
+}
+httpserver();
