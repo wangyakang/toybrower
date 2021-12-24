bc72942696dbea39dd861d165630cf92bee0c139
wangyakang <7782379+wangyakang@user.noreply.gitee.com>
2021-12-24 11:14:58 +0800

    处理返回Body

--- http/http.js
+++ http/http.js
@@ -8,7 +8,7 @@ const { runInThisContext } = require('vm');
  */
 
 class Request {
-    constructor (options = {}) {
+    constructor(options = {}) {
         this.method = options.method || 'get';
         this.path = options.path || '/';
         this.host = options.host || '';
@@ -25,89 +25,199 @@ class Request {
         if (contentType === 'application/json') {
             this.bodyText = JSON.stringify(this.body);
         } else if (contentType === 'application/x-www-form-urlencoded') {
-            this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&');
+            this.bodyText = Object.keys(this.body)
+                .map((key) => `${key}=${encodeURIComponent(this.body[key])}`)
+                .join('&');
         }
         this.headers['Content-Length'] = this.bodyText.length;
     }
-    send (connection) {
-        return new Promise ((resolve, rejects) => {
+    send(connection) {
+        return new Promise((resolve, rejects) => {
             const parser = new ResponseParser();
             if (connection) {
                 connection.write(this.resToString());
             } else {
                 // console.log(this.port, this.host);
-                connection = net.createConnection({
-                    port: this.port,
-                    host: this.host
-                }, _ => {
-                    // console.log(this.resToString(), 'request str');
-                    connection.write(this.resToString());
-                })
+                connection = net.createConnection(
+                    {
+                        port: this.port,
+                        host: this.host,
+                    },
+                    (_) => {
+                        console.log(this.resToString(), 'request str');
+                        connection.write(this.resToString());
+                    }
+                );
             }
-            connection.on('data', data => {
-                console.log(data.toString(), 'response data str');
+            connection.on('data', (data) => {
                 // ...
+
                 parser.receive(data.toString());
+                // "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nDate: Fri, 24 Dec 2021 02:46:43 GMT\r\nConnection: keep-alive\r\nKeep-Alive: timeout=5\r\nTransfer-Encoding: chunked\r\n\r\nb\r\nhello word\n\r\n0\r\n\r\n"
                 if (parser.isFinished) {
-                    resolve(paraser.response);
+                    resolve(parser.response);
                     connection.end();
                 }
-            })
-            connection.on('error', err => {
+            });
+            connection.on('error', (err) => {
                 console.error(err);
                 rejects(err);
                 connection.end();
-            })
-        })
+            });
+        }).catch((error) => {
+            console.log(error);
+        });
     }
-    
-    resToString () { // 拼接request格式
+
+    resToString() {
+        // 拼接request格式
         // GET / HTTP/1.1           // request line
         // Host: www.google.com     // headers
-        // 
+        //
         // name=wyk&sex=1           // body
-        return `${this.method} ${this.path} HTTP/1.1\r\n${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r\n\r\n${this.bodyText}`
+        return `${this.method} ${this.path} HTTP/1.1\r\n${Object.keys(
+            this.headers
+        )
+            .map((key) => `${key}: ${this.headers[key]}`)
+            .join('\r\n')}\r\n\r\n${this.bodyText}`;
     }
 }
 
 class ResponseParser {
-    constructor () {
+    constructor() {
         // 状态控制
-        this.WAITING_STATUS_LINE = false;
-        this.WAITING_STATUS_LINE_END = false;
-        this.WAITING_HEADER_KEY = false;
-        this.WAITING_HEADER_SPACE = false;
-        this.WAITING_HEADER_VALUE = false;
-        this.WAITING_HEADER_END = false;
-        this.WAITING_HEADER_BLOCK = false;
-        this.WAITING_BODY = false;
+        this.WAITING_STATUS_LINE = 0;
+        this.WAITING_STATUS_LINE_END = 1;
+        this.WAITING_HEADER_NAME = 2;
+        this.WAITING_HEADER_SPACE = 3;
+        this.WAITING_HEADER_VALUE = 4;
+        this.WAITING_HEADER_LINE_END = 5;
+        this.WAITING_HEADER_BLOCK_END = 6;
+        this.WAITING_BODY = 7;
 
         // 数据存储
+        this.current = this.WAITING_STATUS_LINE;
         this.statusLine = '';
         this.header = {};
         this.headerName = '';
         this.headerValue = '';
         this.bodyParse = null;
     }
-    receive (string) {
+    get isFinished() {
+        return this.bodyParse && this.bodyParse.isFinished;
+    }
+    get response() {
+        this.statusLine.match(/HTTP\/1.1 (\d+) ([\s\S]+)/);
+        return {
+            statusCode: RegExp.$1,
+            statusText: RegExp.$2,
+            header: this.header,
+            body: this.bodyParse.content.join(''), // to do ....
+        };
+    }
+    receive(string) {
+        // 接收返回值并处理
         for (let i = 0; i < string.length; i++) {
-            this.receiveStart(string.charAt(i));
+            this.receiveChar(string.charAt(i));
         }
-        this.isFinished = this.WAITING_BODY
     }
-    receiveStart (char) {
-        if (this.WAITING_STATUS_LINE) {
-            return receiveLine;
-        } else {
-            
+    receiveChar(char) {
+        // 状态机解析返回值，body to do...
+        if (this.current === this.WAITING_STATUS_LINE) {
+            if (char === '\r') {
+                this.current = this.WAITING_STATUS_LINE_END;
+            } else {
+                this.statusLine += char;
+            }
+        } else if (this.current === this.WAITING_STATUS_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME;
+            }
+        } else if (this.current === this.WAITING_HEADER_NAME) {
+            if (char === ':') {
+                this.current = this.WAITING_HEADER_SPACE;
+            } else if (char === '\r') {
+                this.current = this.WAITING_HEADER_BLOCK_END;
+                // 在heard解析结束后创建Body分析  node环境默认Transfer-Encoding为chunked
+                if (this.header['Transfer-Encoding'] === 'chunked') {
+                    this.bodyParse = new TrunkedBodyParser();
+                }
+            } else {
+                this.headerName += char;
+            }
+        } else if (this.current === this.WAITING_HEADER_SPACE) {
+            if (char === ' ') {
+                this.current = this.WAITING_HEADER_VALUE;
+            }
+        } else if (this.current === this.WAITING_HEADER_VALUE) {
+            if (char === '\r') {
+                this.current = this.WAITING_HEADER_LINE_END;
+                this.header[this.headerName] = this.headerValue;
+                this.headerName = '';
+                this.headerValue = '';
+            } else {
+                this.headerValue += char;
+            }
+        } else if (this.current === this.WAITING_HEADER_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME;
+            }
+        } else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_BODY;
+            }
+        } else if (this.current === this.WAITING_BODY) {
+            // 处理返回body值
+            // to do ......
+            this.bodyParse.receiveChar(char);
         }
-        console.log(char, 'char');
-    }
-    receiveLine (char) {
-        
     }
 }
-
+class TrunkedBodyParser {
+    constructor() {
+        this.WAITING_LENGTH = 0;
+        this.WAITING_LENGTH_LINE_END = 1;
+        this.READING_TRUNK = 2;
+        this.WAITING_NEW_LINER = 3;
+        this.WAITING_NEW_LINER_END = 4;
+        this.length = 0;
+        this.content = [];
+        this.isFinished = false;
+        this.current = this.WAITING_LENGTH;
+    }
+    receiveChar(char) {
+        if (this.isFinished && this.length === 0) return;
+        if (this.current === this.WAITING_LENGTH) {
+            if (char === '\r') {
+                if (this.length === 0) {
+                    this.isFinished = true;
+                }
+                this.current = this.WAITING_LENGTH_LINE_END;
+            } else {
+                this.length *= 16;
+                this.length += parseInt(char, 16);
+            }
+        } else if (this.current === this.WAITING_LENGTH_LINE_END) {
+            if (char === '\n') {
+                this.current = this.READING_TRUNK;
+            }
+        } else if (this.current === this.READING_TRUNK) {
+            this.content.push(char);
+            this.length--;
+            if (this.length === 0) {
+                this.current = this.WAITING_NEW_LINER;
+            }
+        } else if (this.current === this.WAITING_NEW_LINER) {
+            if (char === '\r') {
+                this.current = this.WAITING_NEW_LINER_END;
+            }
+        } else if (this.current === this.WAITING_NEW_LINER_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_LENGTH;
+            }
+        }
+    }
+}
 async function httpserver() {
     let request = new Request({
         method: 'POST',
@@ -115,11 +225,11 @@ async function httpserver() {
         host: '127.0.0.1',
         port: '8001',
         headers: {
-            'Accept': '*'
+            Accept: '*',
         },
         body: {
-            name: 'yk'
-        }
+            name: 'yk',
+        },
     });
     let response = await request.send();
     console.log(11, 'Response back body', response);
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const http = require('http');
-http.createServer((request,response) => {
+http.createServer((request, response) => {
     let body = [];
-    request.on('error',(err) => {
-      console.log(error, 'error');
-    }).on('data', chunk =>{
-      body.push(chunk.toString());
-      console.log(body, 'success');
-    }).on('end' ,() => {
-      body = body.join(' ');
-      console.log(body, 'end');
-      response.writeHead(200,{'Content-Type': 'text/html'});
-      response.end('hello word');
-    })
+    request
+        .on('error', (err) => {
+            console.log(error, 'error');
+        })
+        .on('data', (chunk) => {
+            body.push(chunk.toString());
+            // console.log(body, 'success');
+        })
+        .on('end', () => {
+            body = body.join(' ');
+            console.log(body, 'end');
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end('hello word\n\rwewieowiwoei');
+        });
 }).listen(8001);
 console.log('http server start');
